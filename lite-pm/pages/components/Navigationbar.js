c10fcf32208455caa94fb85e795c07b4179a1ac9
Caleb <csuthe42@uwo.ca>
2021-02-17 18:38:48 -0500

    add team membersection/modal

--- lite-pm/pages/components/Navigationbar.js
+++ lite-pm/pages/components/Navigationbar.js
@@ -1,20 +1,21 @@
-import { Navbar, Nav, NavItem, NavLink, NavbarBrand } from "reactstrap";
+import { Navbar, Nav, NavItem, NavLink, NavbarBrand, Container } from "reactstrap";
+
 
 export default function Navigationbar() {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand className="ml-5">LitePM</NavbarBrand>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink>Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>
-              App
-            </NavLink>
-          </NavItem>
-        </Nav>
+        <Container>
+          <NavbarBrand>LitePM</NavbarBrand>
+          <Nav className="ml-5" navbar>
+            <NavItem>
+              <NavLink>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>App</NavLink>
+            </NavItem>
+          </Nav>
+        </Container>
       </Navbar>
     </div>
   );
