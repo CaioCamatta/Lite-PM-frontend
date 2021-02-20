47a839b51f946af7985f3c8ae4da493e75f379a5
jmaslanka55 <jmaslank@uwo.ca>
2021-02-20 17:04:22 -0500

    Updates project details dynamically

--- lite-pm/pages/components/ProjectDetails.js
+++ lite-pm/pages/components/ProjectDetails.js
@@ -2,28 +2,22 @@ import { Component } from "react";
 import styles from "../../styles/ProjectDetails.module.css";
 
 export default class ProjectDetails extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-      projectName: this.props.projname,
-      projectDescription: this.props.description,
-      projectDuration : this.props.duration,
-      projectLink : `lite-pm.com/project/${this.props.projectLink}`
-    };
+     
   }
+
   render() {
     return (
-      <div>
-        <h1 className={styles.h1}>{this.state.projectName}</h1>
-        <div className={styles.date}>{this.state.projectDuration}</div>
+      <div>       
+        <h1 className={styles.h1}>{this.props.projname}</h1>
+        <div className={styles.date}>{this.props.duration}</div>
         <div className={styles.description}>
-          {this.state.projectDescription}
+          {this.props.description}
         </div>
         <div className="mt-2">
           <div className={styles.projectLink}>Project Link:</div>
-          <div className={styles.link}>
-            {this.state.projectLink}
-          </div>
+          <div className={styles.link}>lite-pm.com/project/{this.props.projectLink}</div>
         </div>
       </div>
     );
