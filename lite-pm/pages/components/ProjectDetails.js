23e6927ac31813d4776f1f1cf3e325e2bf9a4cf1
Caio Coelho <caioz.co@gmail.com>
2021-02-21 21:49:18 -0500

    Fix project link in project details

--- lite-pm/pages/components/ProjectDetails.js
+++ lite-pm/pages/components/ProjectDetails.js
@@ -4,20 +4,21 @@ import styles from "../../styles/ProjectDetails.module.css";
 export default class ProjectDetails extends Component {
   constructor(props) {
     super(props);
-     
   }
 
   render() {
     return (
-      <div>       
+      <div>
         <h1 className={styles.h1}>{this.props.projname}</h1>
         <div className={styles.date}>{this.props.duration}</div>
-        <div className={styles.description}>
-          {this.props.description}
-        </div>
+        <div className={styles.description}>{this.props.description}</div>
         <div className="mt-2">
           <div className={styles.projectLink}>Project Link:</div>
-          <div className={styles.link}>lite-pm.com/project/{this.props.projectLink}</div>
+          <div className={styles.link}>
+            <a href={`https://LitePM.com/project/${this.props.projectLink}`}>
+              https://LitePM.com/project/{this.props.projectLink}
+            </a>
+          </div>
         </div>
       </div>
     );
